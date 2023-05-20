49bb4a43fa7e1b1a849f858e40f631a8a441569e
Tiago <sathoskath@gmail.com>
2023-05-19 23:43:19 -0300

    another attempt

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,7 @@ import { AboutSection } from "./components/About";
 import { IntroSection } from "./components/Intro";
 import { SkillsSection } from "./components/Skills";
 import { ContactSection } from "./components/Contact";
-import { ProjectsSection } from "./components/Projects";
+import ProjectsSection from "./components/Projects";
 import { MainContainer } from "./styles";
 
 export function Main() {
