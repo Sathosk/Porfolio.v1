a0c0c904130d6fc4d2d74e1aad594447fa83e929
Tiago <sathoskath@gmail.com>
2023-05-20 00:17:48 -0300

    test

---
+++ src/components/Main/components/Projeto/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./style";
+
+export function Funcionaporra() {
+    return <Container></Container>;
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,6 +4,7 @@ import { SkillsSection } from "./components/Skills";
 import { ContactSection } from "./components/Contact";
 // import ProjectsSection from "./components/Project";
 import { MainContainer } from "./styles";
+import { Funcionaporra } from "./components/Projeto";
 
 export function Main() {
     return (
@@ -11,6 +12,7 @@ export function Main() {
             <IntroSection />
             <AboutSection />
             <SkillsSection />
+            <Funcionaporra />
             {/* <ProjectsSection /> */}
             <ContactSection />
         </MainContainer>
