eaae4e454a2a42d40277a29fa3bf6754023e4a9a
Tiago <sathoskath@gmail.com>
2023-05-19 22:39:22 -0300

    attempting fix for deployment again

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 import { AboutSection } from "./components/About";
 import { IntroSection } from "./components/Intro";
 import { SkillsSection } from "./components/Skills";
-import { ProjectsSection } from "./components/Projects";
+import { ProjectsSection } from "./components/Projects/index.tsx";
 import { ContactSection } from "./components/Contact";
 import { MainContainer } from "./styles";
 
