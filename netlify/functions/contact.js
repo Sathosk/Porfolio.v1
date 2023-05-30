67de9f1cff38154347c3645eaecb1270897b2fd1
Tiago <sathoskath@gmail.com>
2023-05-29 22:16:20 -0300

    feat: created serverless function for netlify

---
+++ netlify/functions/contact.js
@@ -0,0 +1,31 @@
+import sgMail from "@sendgrid/mail";
+
+// eslint-disable-next-line no-undef
+exports.handler = async (event, context, callback) => {
+    try {
+        console.log(event.body);
+        const { name, email, message } = JSON.parse(event.body);
+        sgMail.setApiKey(import.meta.env.VITE_SENDGRID_API_KEY);
+
+        const mailOptions = {
+            to: import.meta.env.VITE_MAIL_RECIPIENT,
+            from: import.meta.env.VITE_MAIL_RECIPIENT,
+            subject: "Portfolio Contact Submission",
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+        };
+
+        await sgMail.send(mailOptions);
+
+        console.log("Email sent successfully");
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Email sent successfully" }),
+        });
+    } catch (err) {
+        console.error("Error sending email:", err);
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to send email" }),
+        });
+    }
+};
